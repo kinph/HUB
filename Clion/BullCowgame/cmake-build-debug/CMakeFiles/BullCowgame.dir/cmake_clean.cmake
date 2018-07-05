07c78e900f57066dbeadd0939270323f79c951fe
Phillip Kinsley <kinph@msn.com>
2018-07-05 14:15:13 -0700

    moving to IDE folder

---
+++ Clion/BullCowgame/cmake-build-debug/CMakeFiles/BullCowgame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BullCowgame.dir/main.cpp.obj"
+  "BullCowgame.pdb"
+  "BullCowgame.exe"
+  "BullCowgame.exe.manifest"
+  "BullCowgame.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BullCowgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
